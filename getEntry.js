fef9b070283636643de3df010dabc7f51cb22189
JJ Stewart <jstewa12@tufts.edu>
2020-04-23 00:30:56 -0400

    updates

--- getEntry.js
+++ getEntry.js
@@ -4,18 +4,23 @@ const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 const url = "mongodb+srv://jstewa12:JJStewart768@cluster0-l60ir.mongodb.net/test?retryWrites=true&w=majority";
-MongoClient.connect(url, function(err, db) {
-    if (err) {
-        console.log(err);
+http.createServer((request, response) => {
+    if(request.url === '/favicon.ico') {
+        res.status(204);
+        res.end();
         return;
     }
 
-    var dbo = db.db("hw13");
-    var coll = dbo.collection("companies");
-    // var s = coll.find().stream();
-    // s.on("data", function(item) { console.log(item)});
-    // s.on("end", function() {console.log("end of data");});
+    MongoClient.connect(url, function(err, db) {
+        if (err) {
+            console.log(err);
+            return;
+        }
 
-    console.log("Success!");
-    db.close();
-});
+        var dbo = db.db("hw13");
+        var coll = dbo.collection("companies");
+        var s = coll.find().stream();
+        s.on("data", function(item) {console.log(item)});
+        s.on("end", function() {console.log("end of data");});
+    });
+}).listen(8080);
