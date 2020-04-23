3f5a478972aaf5437ececcc0627aeb569eda57f3
JJ Stewart <jstewa12@tufts.edu>
2020-04-23 12:25:08 -0400

    it works'

--- hello_app.js
+++ hello_app.js
@@ -1,26 +1,24 @@
 const http = require('http');
 const MongoClient = require('mongodb').MongoClient;
 
+var port = process.env.PORT || 8080;
 const url = "mongodb+srv://jstewa12:JJStewart768@cluster0-l60ir.mongodb.net/test?retryWrites=true&w=majority";
 http.createServer((request, response) => {
     if(request.url === '/favicon.ico') {
-        res.end();
+        response.end();
         return;
     }
 
-    console.log("Success!");
+    MongoClient.connect(url, function(err, db) {
+        if (err) {
+            console.log(err);
+            return;
+        }
 
-    // MongoClient.connect(url, function(err, db) {
-    //     if (err) {
-    //         console.log(err);
-    //         return;
-    //     }
-    //
-    //     var dbo = db.db("hw13");
-    //     var coll = dbo.collection("companies");
-    //     var s = coll.find().stream();
-    //     s.on("data", function(item) {console.log(item)});
-    //     s.on("end", function() {console.log("end of data");});
-    //     response.send(s);
-    // });
-}).listen(8080);
+        var dbo = db.db("hw13");
+        var coll = dbo.collection("companies");
+        var s = coll.find().stream();
+        s.on("data", function(item) {  console.log(item);  response.write(JSON.stringify(item));  });
+        s.on("end", function() {console.log("end of data"); console.log("Success!"); response.end("Done!");});
+    });
+}).listen(port);
