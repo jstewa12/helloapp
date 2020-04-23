9063ee11aab57c66fb2f3520361868fa4261f3fc
JJ Stewart <jstewa12@tufts.edu>
2020-04-22 21:02:26 -0400

    new stuff

--- getEntry.js
+++ getEntry.js
@@ -1,15 +1,17 @@
+const http = require('http');
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 const url = "mongodb+srv://jstewa12:JJStewart768@cluster0-l60ir.mongodb.net/test?retryWrites=true&w=majority";
-app.get('/', function(req,res) {
-    MongoClient.connect(url, function(err, db) {
-        if (err) {
-            console.log(err);
-            return
-        }
+MongoClient.connect(url, function(err, db) {
+    if (err) {
+        console.log(err);
+        return;
+    }
 
-        res.json(db.companies.find());
-    })
-}).listen(8080);
+    var dbo = db.db("hw13");
+    var coll = dbo.collection("companies");
+    console.log("Success!");
+    db.close();
+});
